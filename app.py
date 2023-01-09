97470be92e86cdab20679fa89dd4d5873449b803
vrombas <71152661+vrombas@users.noreply.github.com>
2023-01-08 19:39:09 -0500

    setting up flask

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from flask import render_template, request, redirect, make_response 
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return make_response(render_template('index.html'))
+
+
+@app.route("/home")
+def homepage():
+    return make_response(render_template("homepage.html")) 
