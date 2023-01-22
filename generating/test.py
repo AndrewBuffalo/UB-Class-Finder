ab899e6e5db1ce013ea01d6518503e0c2a799fae
AndrewBuffalo <badrkadm@buffalo.edu>
2023-01-22 08:22:18 -0500

    turned project into 2 files, one for the backend and one for the front-end

--- generating/test.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime, timedelta,date
-
-beginning = datetime.now()
-print(datetime.strftime(beginning + timedelta(hours = 6), '%H:%M:%S'))
-print(date.today().weekday())
-print( datetime.now().strftime('%A'))
