fc26ff5915ac14273987f69e89d885b4e52f5921
AndrewBuffalo <badrkadm@buffalo.edu>
2023-02-02 21:15:24 -0500

    boostrap green yay

--- client/src/components/Explainer.js
+++ client/src/components/Explainer.js
@@ -1,22 +1,22 @@
 import React from "react";
-import { Alert } from "react-bootstrap";
+import Alert from "react-bootstrap/Alert";
 
 function Explainer() {
   return (
-    <div>
-      <Alert variant="success">
-        <Alert.Heading>So, how does this work?</Alert.Heading>
-        <p>
-          Find a Classroom, an independent and student created tool, performs a
-          comprehensive search of the University at Buffalo's course catalog by
-          scanning information on the location and scheduling of classes. Please
-          note that this tool does not include any private or non-public events,
-          such as club meetings or office hours.
-        </p>
-        <hr />
-        <p className="mb-0">Latest Release: February 2, 2023</p>
-      </Alert>
-    </div>
+    <Alert variant="success">
+      <Alert.Heading>So, how does this work?</Alert.Heading>
+      <p>
+        Find a Classroom, an independent and student created tool, performs a
+        comprehensive search of the University at Buffalo's course catalog by
+        scanning information on the location and scheduling of classes. Please
+        note that this tool does not include any private or non-public events,
+        such as club meetings or office hours.
+      </p>
+      <hr />
+      <p className="mb-0">
+        Latest Release: February 2, 2023
+      </p>
+    </Alert>
   );
 }
 
